236051e0b5ab2986f0fe88c01bcae74c9b1f732a
vkardaras <vasiliskardaras@gmail.com>
2025-11-07 10:02:25 +0200

    update emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -126,6 +126,16 @@ The DWIM behaviour of this command is as follows:
   (setq completion-category-defaults nil)
   (setq completion-category-overrides nil))
 
+(use-package consult)
+
+(use-package embark
+  :bind (("C-." . embark-act)
+         :map minibuffer-local-map
+         ("C-c C-c" . embark-collect)
+         ("C-c C-e" . embark-export)))
+
+(use-package embark-consult)
+
 (use-package savehist
   :ensure nil ; it is built-in
   :hook (after-init . savehist-mode))
@@ -200,36 +210,36 @@ The DWIM behaviour of this command is as follows:
 	  which-key-allow-imprecise-window-fit nil
 	  which-key-separator " → " ))
 
-;;; Evil
-(use-package evil
-    :ensure t
-    :init      ;; tweak evil's configuration before loading it
-    (setq evil-want-integration t  ;; This is optional since it's already set to t by default.
-          evil-want-keybinding nil
-          evil-vsplit-window-right t
-          evil-split-window-below t
-          evil-undo-system 'undo-redo)  ;; Adds vim-like C-r redo functionality
-    (evil-mode))
+;; ;;; Evil
+;; (use-package evil
+;;     :ensure t
+;;     :init      ;; tweak evil's configuration before loading it
+;;     (setq evil-want-integration t  ;; This is optional since it's already set to t by default.
+;;           evil-want-keybinding nil
+;;           evil-vsplit-window-right t
+;;           evil-split-window-below t
+;;           evil-undo-system 'undo-redo)  ;; Adds vim-like C-r redo functionality
+;;     (evil-mode))
 
-(use-package evil-collection
-  :ensure t
-  :after evil
-  :config
-  ;; Do not uncomment this unless you want to specify each and every mode
-  ;; that evil-collection should works with.  The following line is here
-  ;; for documentation purposes in case you need it.
-  ;; (setq evil-collection-mode-list '(calendar dashboard dired ediff info magit ibuffer))
-  (add-to-list 'evil-collection-mode-list 'help) ;; evilify help mode
-  (evil-collection-init))
+;; (use-package evil-collection
+;;   :ensure t
+;;   :after evil
+;;   :config
+;;   ;; Do not uncomment this unless you want to specify each and every mode
+;;   ;; that evil-collection should works with.  The following line is here
+;;   ;; for documentation purposes in case you need it.
+;;   ;; (setq evil-collection-mode-list '(calendar dashboard dired ediff info magit ibuffer))
+;;   (add-to-list 'evil-collection-mode-list 'help) ;; evilify help mode
+;;   (evil-collection-init))
 
-;; Using RETURN to follow links in Org/Evil
-;; Unmap keys in 'evil-maps if not done, (setq org-return-follows-link t) will not work
-(with-eval-after-load 'evil-maps
-  (define-key evil-motion-state-map (kbd "SPC") nil)
-  (define-key evil-motion-state-map (kbd "RET") nil)
-  (define-key evil-motion-state-map (kbd "TAB") nil))
-;; Setting RETURN key in org-mode to follow links
-  (setq org-return-follows-link  t)
+;; ;; Using RETURN to follow links in Org/Evil
+;; ;; Unmap keys in 'evil-maps if not done, (setq org-return-follows-link t) will not work
+;; (with-eval-after-load 'evil-maps
+;;   (define-key evil-motion-state-map (kbd "SPC") nil)
+;;   (define-key evil-motion-state-map (kbd "RET") nil)
+;;   (define-key evil-motion-state-map (kbd "TAB") nil))
+;; ;; Setting RETURN key in org-mode to follow links
+;;   (setq org-return-follows-link  t)
 
 ;;; Keymaps
 ;; Global keymaps
