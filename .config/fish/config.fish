31385088de3b654bf6481d6ecbbda518c96a0403
vkardaras1 <vkardaras@unicorndomain.gr>
2026-01-26 22:17:31 +0200

    update dotfiles

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,3 +2,5 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 starship init fish | source
+
+set -Ux JAVA_HOME /home/vasilis/.sdkman/candidates/java/current/bin/java
